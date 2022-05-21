ff8e8aededf3fba56529d2d7b47f19e8bcedc736
TranVietNam <vietnamtudonghoa98@gmail.com>
2022-05-21 09:33:07 +0700

    Chinh header thay doi khi lan chuot

---
+++ assets/JS/base.js
@@ -0,0 +1,19 @@
+/**
+ *
+/**
+ * Đây là file js dùng chung cho các trang.
+ */
+/////////////////////////
+// Global variables
+const header = document.querySelector('.header');
+
+//////////////////////////
+// Làm header sticky khi bắt đầu lăn con trỏ chuột
+window.addEventListener('scroll', function () {
+    if (window.scrollY !== 0) {
+        header.classList.add('sticky');
+    }
+    else {
+        header.classList.remove('sticky');
+    }
+});
