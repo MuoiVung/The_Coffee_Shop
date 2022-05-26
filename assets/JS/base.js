2a2cfe5f38ad0522e16ae6e2782b71a05112dc8d
TranVietNam <vietnamtudonghoa98@gmail.com>
2022-05-26 23:20:47 +0700

    thêm xử lý login, logout

--- assets/JS/base.js
+++ assets/JS/base.js
@@ -13,6 +13,39 @@ const modalClose = $('.modal__close');
 const modalOverlay = $('.modal__overlay');
 const modalContent = $('.modal__content');
 const body = $('body');
+const modalMains = $$('.modal__main');
+const btnLogin = $('.modal__btn--login');
+
+const modalAccount = $('.modal--account');
+const modalLogin = $('.modal--login');
+const modalRegister = $('.modal--register');
+
+let foundAccount;
+
+const account1 = {
+    email: 'tonggiang@gmail.com',
+    password: '123',
+    name: 'Tống Giang',
+};
+
+const account2 = {
+    email: 'lamxung@gmail.com',
+    password: '345',
+    name: 'Lâm Xung',
+};
+
+const accounts = [account1, account2];
+//////////////////////////
+// classes
+class account {
+    cart = [];
+
+    constructor(email, password, name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
 
 //////////////////////////
 // Tính scrollbar width
@@ -52,3 +85,107 @@ btnUser.addEventListener('click', (e) => {
 modalClose.addEventListener('click', closeModal);
 
 modalOverlay.addEventListener('click', closeModal);
+
+//////////////////////////
+// chức năng login, logout, register
+
+// hiển thị modal account
+const renderAccount = function (foundAccount) {
+    const html = `
+                    <div class="modal__icon">
+                        <img src="./assets/img/login-icon.png" alt="">
+                    </div>
+                    <h3 class="modal__header">Chào mừng</h3>
+                    <p class="modal__account-name">${foundAccount.name},</p>
+                    <ul class="modal__list">
+                        <li class="modal__item">
+                            <a href="#" class="modal__link">
+                                <i class="modal__item-icon fa-solid fa-user"></i>
+                                Thông tin tài khoản
+                            </a>
+                        </li>
+
+                        <li class="modal__item">
+                            <a href="#" class="modal__link">
+                                <i class="modal__item-icon fa-solid fa-cart-shopping"></i>
+                                Giỏ hàng
+                            </a>
+                        </li>
+
+                        <li class="modal__item">
+                            <a href="#" class="modal__link">
+                                <i class="modal__item-icon fa-solid fa-gear"></i>
+                                Cài đặt
+                            </a>
+                        </li>
+                    </ul>
+                    <button class="modal__btn modal__btn--logout">Đăng xuất</button>
+    `;
+
+    modalAccount.innerHTML = html;
+};
+
+// hiển thị modal register
+const showModalRegister = function (modalBtn) {
+    const isInModalLogin = modalBtn.closest('.modal--login');
+    if (!isInModalLogin) return;
+    modalLogin.classList.toggle('modal__main--active');
+    modalRegister.classList.toggle('modal__main--active');
+};
+
+// xử lý login
+const loginHandler = function (modalBtn) {
+    const modalLogin = modalBtn.closest('.modal--login');
+    const email = modalLogin.querySelector('.modal__input[type = "email"]').value;
+    const password = modalLogin.querySelector('.modal__input[type = "password"]').value;
+
+
+    accounts.forEach(account => {
+        if (email === account.email && password === account.password) {
+            foundAccount = account;
+        }
+    });
+
+    if (!foundAccount) {
+        console.log('Tài khoản không tồn tại!');
+        return;
+    }
+
+    modalLogin.classList.toggle('modal__main--active');
+    modalAccount.classList.toggle('modal__main--active');
+
+    renderAccount(foundAccount);
+
+    // xóa dữ liệu ở input sau khi đăng nhập thành công
+    modalLogin.querySelector('.modal__input[type = "email"]').value = '';
+    modalLogin.querySelector('.modal__input[type = "password"]').value = '';
+};
+
+// xử lý logout
+const logoutHandler = function () {
+    modalAccount.classList.toggle('modal__main--active');
+    modalLogin.classList.toggle('modal__main--active');
+};
+
+// xử lý event trong modal content
+modalContent.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!e.target.classList.contains('modal__btn')) return;
+    const modalBtn = e.target;
+    //Ẩn modal đăng nhập, hiển thị modal đăng ký
+    // khi ấn nút đăng ký trong modal login
+    if (modalBtn.classList.contains('modal__btn--register')) {
+        showModalRegister(modalBtn);
+    }
+
+    // Thực hiện đăng nhập
+    if (modalBtn.classList.contains('modal__btn--login')) {
+        loginHandler(modalBtn);
+    }
+
+    // Thực hiện đăng xuất
+    if (modalBtn.classList.contains('modal__btn--logout')) {
+        logoutHandler();
+    }
+});
+
