5de7a9753827e3f993f06c7706658238ed458647
TranVietNam <vietnamtudonghoa98@gmail.com>
2022-05-27 11:57:32 +0700

    Hoàn thiện chức năng đăng ký, đăng nhập. Thêm chức năng này vào tất cả các trang

--- assets/JS/base.js
+++ assets/JS/base.js
@@ -19,6 +19,9 @@ const btnLogin = $('.modal__btn--login');
 const modalAccount = $('.modal--account');
 const modalLogin = $('.modal--login');
 const modalRegister = $('.modal--register');
+const modalError = $('.modal__error');
+const modalInputs = $$('.modal__input');
+
 
 let foundAccount;
 
@@ -26,19 +29,22 @@ const account1 = {
     email: 'tonggiang@gmail.com',
     password: '123',
     name: 'Tống Giang',
+    cart: [],
 };
 
 const account2 = {
     email: 'lamxung@gmail.com',
     password: '345',
     name: 'Lâm Xung',
+    cart: [],
 };
 
-const accounts = [account1, account2];
+let accounts = [];
 //////////////////////////
 // classes
-class account {
+class Account {
     cart = [];
+    isLogging = false;
 
     constructor(email, password, name) {
         this.email = email;
@@ -46,6 +52,18 @@ class account {
         this.name = name;
     }
 }
+
+//////////////////////////
+// Local Storage
+const setLocalStorage = function () {
+    localStorage.setItem('accounts', JSON.stringify(accounts));
+};
+
+const getLocalStorage = function () {
+    accounts = JSON.parse(localStorage.getItem('accounts'));
+    if (!accounts) accounts = [];
+};
+
 
 //////////////////////////
 // Tính scrollbar width
@@ -64,7 +82,18 @@ window.addEventListener('scroll', function () {
 
 //////////////////////////
 // Hiển thị modal đăng nhập khi ấn nút user
+// Reset modal login
+const resetModal = function () {
+    modalError.innerHTML = '';
+    modalInputs.forEach(modalInput => modalInput.value = '');
+};
+
+const showModalError = function (error) {
+    modalError.innerHTML = error;
+};
+
 const closeModal = () => {
+    resetModal();
     modal.classList.add('modal__closing');
     body.classList.remove('open-modal');
 
@@ -76,10 +105,12 @@ const closeModal = () => {
 };
 
 btnUser.addEventListener('click', (e) => {
+    resetModal();
     e.preventDefault();
     body.classList.add('open-modal');
     modal.classList.add('modal__active');
-    header.style.transition = 'inital';
+    header.style.transition = 'initial';
+    setCurrentAccount();
 });
 
 modalClose.addEventListener('click', closeModal);
@@ -123,22 +154,22 @@ const renderAccount = function (foundAccount) {
     `;
 
     modalAccount.innerHTML = html;
+    foundAccount.isLogging = true;
 };
 
-// hiển thị modal register
-const showModalRegister = function (modalBtn) {
-    const isInModalLogin = modalBtn.closest('.modal--login');
-    if (!isInModalLogin) return;
-    modalLogin.classList.toggle('modal__main--active');
-    modalRegister.classList.toggle('modal__main--active');
+// Hiển thị modal main tương ứng
+const renderModalMain = function (modalActive) {
+    modalMains.forEach(modalMain => {
+        modalMain.classList.remove('modal__main--active');
+    });
+
+    modalActive.classList.add('modal__main--active');
 };
 
 // xử lý login
 const loginHandler = function (modalBtn) {
-    const modalLogin = modalBtn.closest('.modal--login');
     const email = modalLogin.querySelector('.modal__input[type = "email"]').value;
     const password = modalLogin.querySelector('.modal__input[type = "password"]').value;
-
 
     accounts.forEach(account => {
         if (email === account.email && password === account.password) {
@@ -147,35 +178,92 @@ const loginHandler = function (modalBtn) {
     });
 
     if (!foundAccount) {
-        console.log('Tài khoản không tồn tại!');
+        showModalError('Tài khoản không tồn tại!');
         return;
     }
 
-    modalLogin.classList.toggle('modal__main--active');
-    modalAccount.classList.toggle('modal__main--active');
+    renderModalMain(modalAccount);
 
     renderAccount(foundAccount);
 
     // xóa dữ liệu ở input sau khi đăng nhập thành công
-    modalLogin.querySelector('.modal__input[type = "email"]').value = '';
-    modalLogin.querySelector('.modal__input[type = "password"]').value = '';
+    resetModal();
+
+    setLocalStorage();
+
 };
 
 // xử lý logout
 const logoutHandler = function () {
-    modalAccount.classList.toggle('modal__main--active');
-    modalLogin.classList.toggle('modal__main--active');
+    renderModalMain(modalLogin);
+    foundAccount.isLogging = false;
+    foundAccount = undefined;
+    setLocalStorage();
+};
+
+// xử lý register
+const registerHandler = function () {
+    const name = modalRegister.querySelector('.modal__input[type = "text"]').value;
+    const email = modalRegister.querySelector('.modal__input[type = "email"]').value;
+    const modalPasswords = modalRegister.querySelectorAll('.modal__input[type = "password"]');
+    const validateEmail = (email) =>
+        email.toLowerCase()
+            .match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+    if (!name.trim()) {
+        showModalError('Vui lòng nhập tên của bạn!');
+        return;
+    }
+
+    if (!validateEmail(email)) {
+        showModalError('Email nhập vào không hợp lệ!');
+        return;
+    }
+
+    for (acc of accounts) {
+        if (acc.email === email) {
+            showModalError('Email này đã tồn tại!');
+            return;
+        }
+    }
+
+    if (!modalPasswords[0].value) {
+        showModalError('Vui lòng nhập mật khẩu');
+        return;
+    }
+
+    if (modalPasswords[0].value !== modalPasswords[1].value) {
+        showModalError('Mật khẩu nhập lại không trùng khớp!');
+        return;
+    }
+
+    const newAccount = new Account(email, modalPasswords[0].value, name.trim());
+    accounts.push(newAccount);
+
+    //Lưu tài khoản mới
+    renderAccount(newAccount);
+    setLocalStorage();
+
+    resetModal();
+    renderModalMain(modalAccount);
+
 };
 
 // xử lý event trong modal content
 modalContent.addEventListener('click', function (e) {
     e.preventDefault();
-    if (!e.target.classList.contains('modal__btn')) return;
+    if (!e.target.classList.contains('modal__btn') &&
+        !e.target.classList.contains('modal__btn-link')) return;
     const modalBtn = e.target;
-    //Ẩn modal đăng nhập, hiển thị modal đăng ký
-    // khi ấn nút đăng ký trong modal login
-    if (modalBtn.classList.contains('modal__btn--register')) {
-        showModalRegister(modalBtn);
+
+    //Ẩn modal login, hiển thị modal register
+    // khi ấn nút đăng ký trong modal login và ngược lại
+    if (modalBtn.classList.contains('modal__btn-link--register')) {
+        renderModalMain(modalRegister);
+    }
+
+    if (modalBtn.classList.contains('modal__btn-link--login')) {
+        renderModalMain(modalLogin);
     }
 
     // Thực hiện đăng nhập
@@ -187,5 +275,30 @@ modalContent.addEventListener('click', function (e) {
     if (modalBtn.classList.contains('modal__btn--logout')) {
         logoutHandler();
     }
+
+    // Thực hiện đăng ký
+    if (modalBtn.classList.contains('modal__btn--register')) {
+        registerHandler();
+    }
 });
 
+const setCurrentAccount = function () {
+    for (acc of accounts) {
+        if (acc.isLogging) {
+            foundAccount = acc;
+            renderModalMain(modalAccount);
+            renderAccount(foundAccount);
+            return;
+        }
+    }
+
+    renderModalMain(modalLogin);
+};
+
+const init = function () {
+    resetModal();
+    getLocalStorage();
+    setCurrentAccount();
+};
+
+init();
