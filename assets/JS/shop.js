067405d22027e82fd0143b4153d85ac2b39a3c37
trieumb <trieu.nv209170@sis.hust.edu.vn>
2022-05-28 18:18:15 +0700

    update second

---
+++ assets/JS/shop.js
@@ -0,0 +1,22 @@
+
+function paginationNext(){
+    console.log("abc")
+    document.getElementById("pagination_2").style.display = "block";
+};
+
+document.getElementById("click-description").onclick = function () {
+    document.getElementById("description").style.display = 'block';
+    document.getElementById("click-description").style.color = "#c7a17a"
+    document.getElementById("trademark").style.display = 'none';
+    document.getElementById("review").style.display = 'none';
+};
+document.getElementById("click-trademark").onclick = function () {
+    document.getElementById("trademark").style.display = 'block';
+    document.getElementById("description").style.display = 'none';
+    document.getElementById("review").style.display = 'none';
+};
+document.getElementById("click-review").onclick = function () {
+    document.getElementById("review").style.display = 'block';
+    document.getElementById("description").style.display = 'none';
+    document.getElementById("trademark").style.display = 'none';
+};
