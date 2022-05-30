35f4fb26d2864526a903006c0d98cb052b96292a
TranVietNam <vietnamtudonghoa98@gmail.com>
2022-05-30 18:01:39 +0700

    thêm giao diện search ở header, thêm chức năng giỏ hàng (ở trang home)

--- assets/JS/shop.js
+++ assets/JS/shop.js
@@ -1,17 +1,17 @@
 
-document.getElementById("click-description").onclick = function () {
-    document.getElementById("description").style.display = 'block';
-    document.getElementById("click-description").style.color = "#c7a17a"
-    document.getElementById("trademark").style.display = 'none';
-    document.getElementById("review").style.display = 'none';
-};
-document.getElementById("click-trademark").onclick = function () {
-    document.getElementById("trademark").style.display = 'block';
-    document.getElementById("description").style.display = 'none';
-    document.getElementById("review").style.display = 'none';
-};
-document.getElementById("click-review").onclick = function () {
-    document.getElementById("review").style.display = 'block';
-    document.getElementById("description").style.display = 'none';
-    document.getElementById("trademark").style.display = 'none';
-};
+// document.getElementById("click-description").onclick = function () {
+//     document.getElementById("description").style.display = 'block';
+//     document.getElementById("click-description").style.color = "#c7a17a"
+//     document.getElementById("trademark").style.display = 'none';
+//     document.getElementById("review").style.display = 'none';
+// };
+// document.getElementById("click-trademark").onclick = function () {
+//     document.getElementById("trademark").style.display = 'block';
+//     document.getElementById("description").style.display = 'none';
+//     document.getElementById("review").style.display = 'none';
+// };
+// document.getElementById("click-review").onclick = function () {
+//     document.getElementById("review").style.display = 'block';
+//     document.getElementById("description").style.display = 'none';
+//     document.getElementById("trademark").style.display = 'none';
+// };
